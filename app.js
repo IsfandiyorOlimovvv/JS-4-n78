a96d67f2f1087a01a489d706901af60be8d77559
IsfandiyorOlimovvv <shoxagaming@gmail.com>
2024-10-18 23:56:09 +0500

    js-4 lesson

---
+++ app.js
@@ -0,0 +1,17 @@
+// let text = prompt("hohlagan sozingizi yozing");
+// let reverseText = text.split('').reverse().join('');
+// console.log(reverseText.toUpperCase());
+
+
+// let mashq = prompt("Gapni kiriting");
+// let wordCount = mashq.split(" ").length;
+// console.log("Sozlar soni", wordCount);
+
+
+// let tugilganKunSiz = new Date(prompt("Tugilgan kuningizni kiriting"));
+// let tugilganKunDost = new Date(prompt("Dostingizning tugilgan kunini kiriting"));
+
+// let vaqtFarqi = tugilganKunSiz - tugilganKunDost;
+// let kunlarFarqi = vaqtFarqi / (1000 * 60 * 60 * 24);
+
+// console.log("Siz va dostingiz orasidagi kunlar farqi", kunlarFarqi);
